21f589e399b575675e1e3a9ecfe07abec59ddb13
Anton L-A <antonmax7@hotmail.com>
2014-11-10 14:33:17 +0100

    main

--- compress/main.cpp
+++ compress/main.cpp
@@ -5,11 +5,29 @@
 //  Created by Robin on 10/11/14.
 //  Copyright (c) 2014 FRAD. All rights reserved.
 //
-
+#include <string.h>
 #include <iostream>
+using namespace std;
+
 
 int main(int argc, const char * argv[]) {
+	char* path;
+	
+
     // insert code here...
     std::cout << "Hello, World!\n";
+	cout<<"type a pathway to a bmp-file to compress or decompress:  ";
+	cin>>path;
+	cin.ignore();
+	
+	if(path[sizeof(path)] == 'p')
+	{
+	}
+	else if(path[sizeof(path)] == 'D')
+	{
+	}
+
+	else cout<<"this ain't no file a kno'"
+
     return 0;
-}
+};
