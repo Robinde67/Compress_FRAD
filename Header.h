982340bc702f4c8bc7bc3de3889aa069545898f3
Robinde67 <a>
2014-11-10 16:17:52 +0100

    last attempt

---
+++ Header.h
@@ -0,0 +1,13 @@
+//
+//  Header.h
+//  compress
+//
+//  Created by Robin on 10/11/14.
+//  Copyright (c) 2014 FRAD. All rights reserved.
+//
+
+#ifndef compress_Header_h
+#define compress_Header_h
+
+
+#endif
--- compress/main.cpp
+++ compress/main.cpp
@@ -5,11 +5,101 @@
 //  Created by Robin on 10/11/14.
 //  Copyright (c) 2014 FRAD. All rights reserved.
 //
+// main.c
 
+#include <stdint.h>
+#include <string.h>
+#include <stdio.h>
 #include <iostream>
+#include "Header.h"
 
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
+#define SIZE 196608
+
+#pragma pack(push, 1)
+struct BMPHeader
+{
+    uint16_t signature;		// 0x4D42
+    uint32_t size;				// unreliable
+    uint16_t res0;				// reserved 0
+    uint16_t res1;				// reserved 0
+    uint32_t offset;			// to start of image
+    uint32_t hdrsize;			// 40
+    uint32_t width;
+    uint32_t height;
+    uint16_t planes;			// 1
+    uint16_t bpp;					// 1, 4, 8 or 24
+    uint32_t compress;		// 0 = none, 1 = rle-8, 2=rle-4
+    uint32_t total;				// size in bytes incl padding
+    uint32_t h_res;
+    uint32_t v_res;
+    uint32_t num_color;
+    uint32_t imp_color;
+};
+#pragma pack(pop)
+
+struct pixelstack
+{
+    int pixelnum;
+    char R,G,B;
+};
+
+int main()
+{
+    FILE* file;
+    struct BMPHeader header;
+    char buffer[SIZE], *pixels;
+    
+    file = fopen("/Users/robin/Documents/spelprogrammering/Compress_FRAD/compress/image.bmp", "rb");
+    fread(buffer, 1, SIZE, file);
+    fclose(file);
+    
+    memcpy(&header, buffer, sizeof(struct BMPHeader));
+    pixels = buffer + header.offset;
+    FILE* newfile;
+    newfile = fopen("/Users/robin/Documents/spelprogrammering/Compress_FRAD/compress/image.FRAD", "wb");
+    fwrite(&header, 1, sizeof(struct BMPHeader), newfile);
+    struct pixelstack stack{1,pixels[0], pixels[1], pixels[2]};
+    for (int i=3; i < SIZE; i+=3)
+    {
+        if(pixels[i] != pixels[i-3] || pixels[i+1] != pixels[i-2] || pixels[i+2] != pixels[i-1])
+        {
+            fwrite(&stack, 1, sizeof(struct pixelstack), newfile);
+            stack.R = pixels[i];
+            stack.G = pixels[i+1];
+            stack.B = pixels[i+2];
+            stack.pixelnum = 1;
+        }
+        else
+        {
+            stack.pixelnum++;
+        }
+    }
+    fwrite(&stack, 1, sizeof(struct pixelstack), newfile);
+    fclose(newfile);
+    /*struct BMPHeader headerd;
+    void* stackd;
+    char* bufferd[sizeof(newfile)];
+    
+    FILE* decompfile;
+    newfile = fopen("/Users/robin/Documents/spelprogrammering/Compress_FRAD/compress/image.FRAD", "rb");
+    
+    
+    decompfile = fopen("/Users/robin/Documents/spelprogrammering/Compress_FRAD/compress/imagedecomp.bmp", "wb");
+    
+    
+    
+    fread(bufferd, 1, sizeof(newfile), newfile);
+    memcpy(&headerd, bufferd, sizeof(struct BMPHeader));
+    
+    stackd = bufferd + header.offset;*/
+    
+    
+    
+    
+    // vi hann inte längre
+    
+    
+    
+    std::cin.get();
     return 0;
 }
